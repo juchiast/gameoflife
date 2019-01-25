6101f12599b6e49e57bab2507a4fbe2b4faac3d4
Do Duy <juchiast@gmail.com>
2019-01-25 22:18:37 +0700

    bench

--- gol/benches/bench.rs
+++ gol/benches/bench.rs
@@ -1,14 +1,12 @@
 #![feature(test)]
 
-extern crate gol;
 extern crate test;
 
-use gol::map;
 use test::Bencher;
 
 #[bench]
 fn benchmark_with_acorn(b: &mut Bencher) {
-    let mut map = map::Map::acorn();
+    let mut map = gol::Map::acorn();
     for _ in 0..4408 {
         map.next_generation();
     }
